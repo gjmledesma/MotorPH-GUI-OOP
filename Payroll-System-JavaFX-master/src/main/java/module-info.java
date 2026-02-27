641a241634e2eae0a71971f7a3ecdbf33b9367fc
Gabriel Ledesma <lr.gjledesma@mmdc.mcl.edu.ph>
2026-02-27 22:19:59 +0800

    Delete Payroll-System-JavaFX-master/src/main/java directory

--- Payroll-System-JavaFX-master/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.motorphui {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-    opens org.example.motorphui to javafx.fxml;
-    exports org.example.motorphui;
-}
