cbc47962c1bf3923b21b861186ee35746fc2a8ba
Gabriel Ledesma <lr.gjledesma@mmdc.mcl.edu.ph>
2026-02-27 22:26:11 +0800

    Delete Payroll-System-JavaFX-master/src/main/src/main directory

--- Payroll-System-JavaFX-master/src/main/src/main/java/org/example/motorphui/model/LeaveRequest.java
+++
@@ -1,7 +0,0 @@
-package org.example.motorphui.model;
-
-import org.example.motorphui.dao.ImpLeaveRequest;
-
-public class LeaveRequest implements ImpLeaveRequest {
-    
-}
--- Payroll-System-JavaFX-master/src/main/src/main/java/org/example/motorphui/ui/DeleteEmployee.java
+++
@@ -1,4 +0,0 @@
-package org.example.motorphui.ui;
-
-public class DeleteEmployee {
-}
